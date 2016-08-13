a23745b43fe0f00110eec3210b88f17bf5647e7e
andreuramos <andreu.ramos.amengual@gmail.com>
2016-08-13 12:10:01 +0200

    first html rendering

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,14 +1,49 @@
+var CommentList = React.createClass({
+	render: function(){
+		return (
+			<div className="commentList">
+				asdasd
+			</div>
+		);
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function(){
+		return (
+			<div className="commentForm">
+				Comment Form
+			</div>
+		)
+	}
+})
+
 var CommentBox = React.createClass({
 	render: function(){
 		return (
 			<div className="CommentBox">
 				Civitfun Hackathon: React comment board.
+				<CommentList />
+				<CommentForm /> 
+			</div>
+		);
+	}
+});
+
+var Comment = React.createClass({
+	render: function(){
+		return(
+			<div className="Comment">
+				<h2 className="commentAuthor">
+					{this.prop.author}
+				</h2>
+				{this.prop.children}
 			</div>
 		);
 	}
 });
 
 ReactDOM.render(
-	<CommentBox />
+	<CommentBox />,
 	document.getElementById('content')
 );
